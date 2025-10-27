2b096658edacb3649abc21a76d09f20f8aa98fe6
jiakang.peng <jiakang.peng@rgare.com>
2025-10-27 10:17:06 +0800

    chore: add script for auto generate folder

---
+++ customer_script.zsh
@@ -0,0 +1,44 @@
+#!/usr/bin/env zsh
+
+function create_docker_compose() {
+    local DOCKER_COMPOSE_NAME=$1
+    local dir="./docker-compose.yaml"
+    if [ ! -f "$dir" ]; then
+        touch "$dir"
+    fi
+    cat >"$dir" <<EOF
+services:
+  ${DOCKER_COMPOSE_NAME}:
+    image: xxxx
+    ports:
+      - "xxxx:xxxx"
+    volumes:
+      - volumes/xxxx:xxxx
+    env_file:
+      - .env
+EOF
+}
+
+function create_env_file() {
+    local dir="./.env.sample"
+    if [ ! -f "$dir" ]; then
+        touch "$dir"
+    fi
+    echo "EXAMPLE_PARAMETER=xxx" >> "$dir"
+}
+
+function initial_docker_compose() {
+    get_input "📝 Enter the name: "
+    DOCKER_COMPOSE_NAME=$result
+    if [ -d "./${DOCKER_COMPOSE_NAME}" ]; then
+        echo "❗ Directory ./${DOCKER_COMPOSE_NAME} already exists. Exiting."
+        return 1
+    fi
+    mkdir "./${DOCKER_COMPOSE_NAME}"
+    cd "./${DOCKER_COMPOSE_NAME}"
+    create_docker_compose ${DOCKER_COMPOSE_NAME}
+    create_env_file
+    if [ ! -d "./volumes" ]; then
+        mkdir "./volumes"
+    fi
+}
